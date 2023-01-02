3b9de23a78149f9b85b3b73db49882f8e838ed40
CptMcTawish <73851144+keenOnFrontEnd@users.noreply.github.com>
2023-01-02 11:45:10 +0200

    setting up the routing

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const path = require('path')
 const model = require('./models/models')
+const router = require('./routers/index')
+const errorHandler = require("./middleware/ErrorHandlingMiddleware")
 
 
 const app = express()
@@ -13,8 +15,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname,'static')))
 app.use(fileUpload({}))
+app.use("/api",router)
 
-
+app.use(errorHandler)
 const start = async () => {
     try {
         await sequelize.authenticate()
