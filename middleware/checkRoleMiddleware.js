195c2178379535a604f137bc8f3c53dd3da525fa
CptMcTawish <73851144+keenOnFrontEnd@users.noreply.github.com>
2023-01-02 11:43:49 +0200

    middleware creating

---
+++ middleware/checkRoleMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = function (role) {
+    return function (req, res, next) {
+        if (req.method === "OPTIONS") {
+            next()
+        }
+        try {
+            const token = req.headers.authorization.split(' ')[1] // bearer dadad
+            if (!token) {
+                res.status(401).json({ message: "Not authorize" })
+            }
+            const decode = jwt.verify(token, process.env.SECRET_KEY)
+            if(decode.role !== role) {
+                return res.status(403).json({ message: "Not enought permissions for this action" })
+            }
+            req.user = decode
+            next()
+        } catch (e) {
+            res.status(401).json({ message: "Not authorize" })
+        }
+    }
+}
